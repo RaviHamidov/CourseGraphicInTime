24061523f5b3be5967b40c86dcf646387b7b38cd
ravihamidov <ravihamidov42@gmail.com>
2022-08-30 14:56:39 +0400

    Changes

---
+++ assets/js/example.js
@@ -0,0 +1,30 @@
+function loadHTML(myDivId, url) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                document.getElementById(myDivId).innerHTML =
+                    xmlhttp.responseText;
+                var allScripts = document
+                    .getElementById(myDivId)
+                    .getElementsByTagName("script");
+                for (var n = 0; n < allScripts.length; n++) {
+                    eval(allScripts[n].innerHTML); //run script inside div generally not a good idea but these scripts are anyways intended to be executed.
+                }
+            } else {
+                alert("Error");
+            }
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+loadHTML("mydiv", "template1.html");
